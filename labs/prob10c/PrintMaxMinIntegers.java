476d0acf30583f458ec2d9e780cda1d314f8d0fb
Toe Toe Aung <toetoeag@gmail.com>
2024-07-14 10:02:08 -0500

    MPP Lesson9 Problem 10

--- labs/prob10c/PrintMaxMinIntegers.java
+++ labs/prob10c/PrintMaxMinIntegers.java
@@ -1,2 +1,16 @@
-package lesson9.labs.prob10c;public class PrintMaxMinIntegers {
+package lesson9.labs.prob10c;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class PrintMaxMinIntegers {
+    public static void main(String[] args) {
+        Stream<Integer> myIntStream= Stream.of(1,2,3,4,5,4,9,-1,10,2,1);
+        IntSummaryStatistics stats = myIntStream.collect(Collectors.summarizingInt(Integer::intValue));
+
+        int max = stats.getMax();
+        int min = stats.getMin();
+        System.out.println("Maximum Value :" + max);
+        System.out.println("Minimum Value :" + min);
+    }
 }
